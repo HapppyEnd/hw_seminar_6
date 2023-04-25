cf064e041e1136673973c181985c4c78d43915a2
Alena Savostyanova <el.savostyanova90@yandex.ru>
2023-04-25 17:43:27 +0300

    Edit task 32.

--- task_32.py
+++ task_32.py
@@ -1,15 +1,16 @@
+from random import randint
+
 """Задача 32: Определить индексы элементов массива (списка),
 значения которых принадлежат заданному диапазону (т.е. не
 меньше заданного минимума и не больше заданного
 максимума)"""
 
-list_1 = [-5, 9, 0, 3, -1, -2, 1,
-          4, -2, 10, 2, 0, -9, 8,
-          10, -9, 0, -5, -5, 7]
+elements = int(input('Введите количество элементов списка: '))
+list_1 = [randint(-10, 10) for i in range(elements)]
 
 min_number = int(input('Введите первое число: '))
 max_number = int(input('Введите второе число: '))
 
 for i in range(len(list_1)):
-    if min_number < list_1[i] < max_number:
+    if min_number <= list_1[i] <= max_number:
         print(i, end=' ')
